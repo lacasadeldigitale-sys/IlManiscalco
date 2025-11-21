490f133f79fa1a9b5f8b237403787c723ff865a5
lacasadeldigitale-sys <lacasadeldigitale@gmail.com>
2025-11-21 11:41:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 # Configurazione pagina
 st.set_page_config(
     page_title="Il Maniscalco - Podologia Bovina",
-    page_icon="logo icona.png",
+    page_icon="logoicona.png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
